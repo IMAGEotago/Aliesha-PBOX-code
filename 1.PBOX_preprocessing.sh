8076f57bd7bd1d8001524074c37a85394748b918
alieshakemp <97994785+alieshakemp@users.noreply.github.com>
2023-12-05 10:19:05 +1300

    Create 1.PBOX_preprocessing.sh
    This bash script runs BET on struct.nii.gz for each subject based on values specified in PBOX_bet-values.csv and runs the registration for each subject using the registration template.

---
+++ 1.PBOX_preprocessing.sh
@@ -0,0 +1,35 @@
+# SET THE PATHS - ADJUST THIS SECTION ONLY
+
+data=/data/kemal206/PBOX_rest_data
+scripts=/home/kemal206/PBOX_scripts
+
+############################################################################################
+
+# SPECIFY THE VARIABLES
+
+# Set PPIDs to analyse
+subj=(subj001 subj002 subj003 subj004 subj005 subj006 subj007 subj008 subj009 subj010 subj011 subj012 subj013 subj014 subj015 subj016 subj017 subj018 subj019 subj020 subj021 subj022 subj023 subj024 subj025 subj026 subj027 subj028 subj029 subj030 subj031 subj032 subj033 subj034 subj035 subj036 subj037 subj038 subj039 subj040)
+
+# Specify the template
+template=${scripts}/PBOX_reg_template.fsf
+
+############################################################################################
+
+# BET the structural image
+#echo "Beginning to run BET on the structural image for each subject"
+#for x in ${subj[@]}; do
+	#echo "Running BET on structural image for ${x}"
+	#bet ${data}/${x}/${x}_struct.nii.gz ${data}/${x}/${x}_struct_brain.nii.gz
+#done
+
+# Run the registrations using the template
+echo "Beginning to run the registration for each subject using the template"
+for x in ${subj[@]}; do
+	echo "Running the registration for ${x}"
+	cat ${template} | sed s:INPUTDATAFILE:${data}/${x}/${x}_rest_clean.nii.gz:g \
+		| sed s:STRUCT:${data}/${x}/${x}_struct_brain.nii.gz:g \
+		| sed s:FSLSTANDARD:/home/fsl-6.0.4/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz:g \
+		| sed s:OUTPUTDIR:${data}/${x}/${x}_reg.feat:g > \
+		${scripts}/design_files/PBOX_${x}_reg.fsf
+	feat ${scripts}/design_files/PBOX_${x}_reg.fsf &
+done
